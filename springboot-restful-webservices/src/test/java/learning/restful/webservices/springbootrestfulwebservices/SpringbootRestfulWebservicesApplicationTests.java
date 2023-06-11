b9bfacee01e6d079c27d6a03fedc8ae0d75c3f7f
Pradip Vala <vala.pradip14@gmail.com>
2023-06-11 17:02:54 +0530

    initial commit

---
+++ springboot-restful-webservices/src/test/java/learning/restful/webservices/springbootrestfulwebservices/SpringbootRestfulWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.restful.webservices.springbootrestfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestfulWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
